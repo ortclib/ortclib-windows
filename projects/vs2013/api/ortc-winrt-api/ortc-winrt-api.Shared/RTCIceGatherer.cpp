e6a610061edf54e03bcd54f8412402a5e9153a47
unknown <lazic_bo@yahoo.com>
2015-11-18 17:31:26 +0100

    ORTC WinRT API project setup and prototype call

---
+++ projects/vs2013/api/ortc-winrt-api/ortc-winrt-api.Shared/RTCIceGatherer.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "RTCIceGatherer.h"
+
+using namespace ortc_winrt_api;
+using namespace Platform;
+
+using namespace zsLib;
+
+Windows::UI::Core::CoreDispatcher^ g_windowDispatcher;
+
+RTCIceGatherer::RTCIceGatherer(RTCIceGatherOptions^ options)
+{
+  zsLib::String url = zsLib::String("stun:") + "stun.vline.com";
+
+  ortc::IICEGatherer::Server server;
+  server.mURLs.push_back(url);
+  ortc::IICEGatherer::Options optionsNative;
+  optionsNative.mICEServers.push_back(server);
+  mNativePointer = &IICEGatherer::create(NULL, optionsNative);
+}
+
+RTCIceGatherer^ RTCIceGatherer::createAssociatedGatherer()
+{
+  RTCIceGatherer^ ret = nullptr;
+  return ret;
+}
---
+++ projects/vs2013/api/ortc-winrt-api/ortc-winrt-api.Shared/RTCIceGatherer.h
@@ -0,0 +1,90 @@
+﻿#pragma once
+
+#include <ortc/types.h>
+#include <ortc/IICEGatherer.h>
+
+using namespace ortc;
+
+namespace ortc_winrt_api
+{
+
+  //public class IIceGathererDelegateWrapper
+  //{
+  //public:
+
+  //  virtual void onICEGathererStateChanged(
+  //    IICEGathererPtr gatherer,
+  //    IICEGatherer::States state
+  //    );
+
+  //  //virtual void onICEGathererLocalCandidate(
+  //  //  IICEGathererPtr gatherer,
+  //  //  CandidatePtr candidate
+  //  //  );
+
+  //  //virtual void onICEGathererLocalCandidateComplete(
+  //  //  IICEGathererPtr gatherer,
+  //  //  CandidateCompletePtr candidate
+  //  //  );
+
+  //  //virtual void onICEGathererLocalCandidateGone(
+  //  //  IICEGathererPtr gatherer,
+  //  //  CandidatePtr candidate
+  //  //  );
+
+  //  //virtual void onICEGathererError(
+  //  //  IICEGathererPtr gatherer,
+  //  //  ErrorCode errorCode,
+  //  //  String errorReason
+  //  //  );
+
+  //};
+  public ref class RTCIceServer sealed
+  {
+    std::vector<Platform::String^> mURLs;
+    Platform::String^     mUserName;
+    Platform::String^     mCredential;
+  };
+
+  public enum class RTCIceGatherPolicy
+  {
+    IceGatherPolicy_All,
+    IceGatherPolicy_Nohost,
+    IceGatherPolicy_Relay
+  };
+
+  public ref class RTCIceGatherOptions sealed
+  {
+    RTCIceGatherPolicy     gatherPolicy;
+    std::vector<RTCIceServer^> iceServers;
+  };
+
+	public ref class RTCIceGatherer sealed
+	{
+
+    //friend class RTCIceGathererDelegateWrapper;
+	public:
+    RTCIceGatherer(RTCIceGatherOptions^ options);
+    RTCIceGatherer^ createAssociatedGatherer();
+
+  private:
+    IICEGathererPtr* mNativePointer;
+    IICEGathererDelegatePtr* mNativeDelegatePointer;
+
+  public:
+    ///// <summary>
+    ///// A remote peer has opened a data channel.
+    ///// </summary>
+    //event onICEGathererStateChanged^ onICEGathererStateChanged;
+
+    ///// <summary>
+    ///// New connection health stats are available.
+    ///// </summary>
+    //event RTCPeerConnectionHealthStatsDelegate^ OnConnectionHealthStats;
+
+    ///// <summary>
+    ///// Webrtc statistics report is ready <see cref="RTCStatsReports"/>.
+    ///// </summary>
+    //event RTCStatsReportsReadyEventDelegate^  OnRTCStatsReportsReady;
+	};
+}
