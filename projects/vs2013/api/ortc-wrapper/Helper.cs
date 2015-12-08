bc89e9b6e39a894f7e650838db1d9787b891216c
Sergej Jovanovic <sergej@hookflash.com>
2015-12-08 23:42:48 +0100

    Added ortc-api-test project for vs2015

---
+++ projects/vs2013/api/ortc-wrapper/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ortc_winrt_api;
+
+namespace OrtcWrapper
+{
+    class Helper
+    {
+        public static RTCIceCandidate toWrapperIceCandidate(ortc_winrt_api.RTCIceCandidate iceCandidate)
+        {
+            StringBuilder sb = new StringBuilder();
+
+
+            var ret = new RTCIceCandidate("","",0);
+
+            return ret; 
+        }
+    }
+}
