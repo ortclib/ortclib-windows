eda3ac52efa7b28d87cb815b846fa8640bc638bd
Sergej Jovanovic <sergej@hookflash.com>
2016-03-22 15:11:21 +0100

    Added ortc device enumerator project

--- projects/samples/ortc-device-enumerator/ortc-device-enumerator.Windows.Universal/MainPage.xaml.cs
+++ projects/samples/ortc-device-enumerator/ortc-device-enumerator.Windows.Universal/MainPage.xaml.cs
@@ -12,7 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using ortc_winrt_api;
+using org.ortc;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace ortc_device_enumerator.Windows.Universal
