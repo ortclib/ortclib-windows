0f905fd8c5941bfb56241cf1ec685bf341a190e2
Robin Raymond <robin@hookflash.com>
2016-04-21 19:07:45 -0400

    - added documentation around DTMF sender https://github.com/hookflash/project-winrt/issues/17

--- api/org.ortc/org.ortc.Shared/RTCDtmfSender.h
+++ api/org.ortc/org.ortc.Shared/RTCDtmfSender.h
@@ -17,10 +17,18 @@ namespace org
       ZS_DECLARE_CLASS_PTR(RTCDtmfSenderDelegate)
     } // namespace internal
 
+    /// <summary>
+    /// The OnToneChange event uses the RTCDTMFToneChangeEvent interface.
+    /// </summary>
     public ref struct RTCDTMFToneChangeEvent sealed
     {
       friend class internal::RTCDtmfSenderDelegate;
 
+      /// <summary>
+      /// Gets the tone attribute contains the character for the tone that has
+      /// just begun playout (see InsertDTMF()). If the value is the empty
+      /// string, it indicates that the previous tone has completed playback.
+      /// </summary>
       property Platform::String^ Tone
       {
         Platform::String^ get() { return _tone; }
@@ -32,6 +40,10 @@ namespace org
 
     public delegate void RTCDtmfSenderOnToneChangeDelegate(RTCDTMFToneChangeEvent^ evt);
 
+    /// <summary>
+    /// An RTCDtmfSender instance allows sending DTMF tones to/from the remote
+    /// peer, as per [RFC4733].
+    /// </summary>
     public ref class RTCDtmfSender sealed
     {
       friend class internal::RTCDtmfSenderDelegate;
@@ -40,40 +52,89 @@ namespace org
       RTCDtmfSender();
 
     public:
+      /// <summary>
+      /// </summary>
       RTCDtmfSender(RTCRtpSender^ sender);
 
+      /// <summary>
+      /// The InsertDTMF() method is used to send DTMF tones. Since DTMF tones
+      /// cannot be sent without configuring the DTMF codec, if InsertDTMF()
+      /// is called prior to sender.Send(parameters), or if
+      /// sender.Send(parameters) was called but parameters did not include
+      /// the DTMF codec, throw an InvalidStateError exception.
+      /// </summary>
       [Windows::Foundation::Metadata::DefaultOverloadAttribute]
       void                        InsertDtmf(Platform::String^ tones);
+      /// <summary>
+      /// The InsertDTMF() method is used to send DTMF tones. Since DTMF tones
+      /// cannot be sent without configuring the DTMF codec, if InsertDTMF()
+      /// is called prior to sender.Send(parameters), or if
+      /// sender.Send(parameters) was called but parameters did not include
+      /// the DTMF codec, throw an InvalidStateError exception.
+      /// </summary>
       [Windows::Foundation::Metadata::OverloadAttribute("InsertDtmfWithDuration")]
       void                        InsertDtmf(Platform::String^ tones, uint64 duration);
+      /// <summary>
+      /// The InsertDTMF() method is used to send DTMF tones. Since DTMF tones
+      /// cannot be sent without configuring the DTMF codec, if InsertDTMF()
+      /// is called prior to sender.Send(parameters), or if
+      /// sender.Send(parameters) was called but parameters did not include
+      /// the DTMF codec, throw an InvalidStateError exception.
+      /// </summary>
       [Windows::Foundation::Metadata::OverloadAttribute("InsertDtmfWithDurationAndGap")]
       void                        InsertDtmf(Platform::String^ tones, uint64 duration, uint64 interToneGap);
 
+      /// <summary>
+      /// Gets whether the RTCDtmfSender is capable of sending DTMF.
+      /// </summary>
       property Platform::Boolean CanInsertDtmf
       {
         Platform::Boolean get();
       }
 
+      /// <summary>
+      /// Gets the RTCRtpSender instance.
+      /// </summary>
       property RTCRtpSender^ Sender
       {
         RTCRtpSender^ get();
       }
 
+      /// <summary>
+      /// Gets the toneBuffer attribute returns a list of the tones remaining
+      /// to be played out.
+      /// </summary>
       property Platform::String^ ToneBuffer
       {
         Platform::String^ get();
       }
 
+      /// <summary>
+      /// Get the duration attribute returns the current tone duration value
+      /// in milliseconds. This value will be the value last set via the
+      /// InsertDTMF() method, or the default value of 100 ms if
+      /// InsertDTMF() was called without specifying the duration.
+      /// </summary>
       property uint64 Duration
       {
         uint64 get();
       }
 
+      /// <summary>
+      /// Gets the interToneGap attribute returns the current value of the
+      /// between-tone gap. This value will be the value last set via the
+      /// InsertDTMF() method, or the default value of 70 ms if InsertDTMF()
+      /// was called without specifying the interToneGap.
+      /// </summary>
       property uint64 InterToneGap
       {
         uint64 get();
       }
 
+      /// <summary>
+      /// The OnToneChange event handler uses the RTCDTMFToneChangeEvent
+      /// interface to return the character for each tone as it is played out.
+      /// </summary>
       event RTCDtmfSenderOnToneChangeDelegate^       OnToneChange;
 
     private:
