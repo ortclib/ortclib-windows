a73d158f01a4aecc8db98bea70a0ba7e918bfae4
Sergej <sergej@gnedo.com>
2016-11-21 13:58:44 +0100

    Renamed org.ortc to Org.Ortc

--- samples/org.ortc.Test/Properties/AssemblyInfo.cs
+++ samples/org.ortc.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("org.ortc.Test")]
+[assembly: AssemblyTitle("Org.Ortc.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("org.ortc.Test")]
+[assembly: AssemblyProduct("Org.Ortc.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
