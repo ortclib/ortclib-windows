4803fd6cc5d8d0c7cca00a9304a288bf6b6a007d
Sergej Jovanovic <sergej@gnedo.com>
2017-05-25 13:56:29 +0200

    Added OrtcCrossSample cross platfrom project

---
+++ samples/OrtcCrossSample/OrtcCrossSample/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Org.Ortc;
+using Org.Ortc.Adapter;
+
+namespace OrtcCrossSample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
