ee93736a391a415aa3bd82b00565a928b5cf34d0
Sergej Jovanovic <sergej@gnedo.com>
2017-05-30 18:54:10 +0200

    Finished iOS example

--- samples/OrtcCrossSample/OrtcCrossSample/MainPage.xaml.cs
+++ samples/OrtcCrossSample/OrtcCrossSample/MainPage.xaml.cs
@@ -6,19 +6,55 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Org.Ortc;
 using Org.Ortc.Adapter;
+using System.Collections.ObjectModel;
 
 namespace OrtcCrossSample
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        public static ObservableCollection<MediaDeviceInfo> Devices = new ObservableCollection<MediaDeviceInfo>();
+
+        public MainPage()
 		{
-			InitializeComponent();
+            InitializeComponent();
 		}
+
+	    private void EnumerateDevices()
+	    {
+	        try
+	        {
+	            var asyncOp = MediaDevices.EnumerateDevices();
+
+	            asyncOp.ContinueWith(op =>
+	            {
+	                try
+	                {
+	                    var devices = op.Result;
+	                    foreach (var deviceInfo in devices)
+	                    {
+	                        Devices.Add(deviceInfo);
+                        }
+	                }
+	                catch (Exception e)
+	                {
+	                    //string str = "Obtaining results of the enumeration has failed.";
+	                    DisplayAlert("Error", "Obtaining results of the enumeration has failed.", "OK");
+                    }
+                });
+	        }
+	        catch (Exception e)
+	        {
+	            //string str = "Device enumeration has failed.";
+	            DisplayAlert("Error", "Device enumeration has failed.", "OK");
+            }
+	        
+	    }
 
         private void Button_Click_EnumerateDevices(object sender, EventArgs e)
         {
-
+            DisplayAlert("Alert", "Enumeration started!", "OK");
+            Devices.Clear();
+            this.EnumerateDevices();
         }
     }
 
